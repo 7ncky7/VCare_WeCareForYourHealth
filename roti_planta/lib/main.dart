1f7ae4424a002503decadd9c03253c1a0f6d7023
7ncky7 <yunqi0729@gmail.com>
2025-03-26 02:52:03 +0800

    Update merge2.py

--- roti_planta/lib/main.dart
+++ roti_planta/lib/main.dart
@@ -3,8 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:roti_planta/pages/landing_page.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:roti_planta/pages/landing_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
